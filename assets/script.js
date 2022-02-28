28a58b84d388d9c2645322ba9b14fd3c61e6cff0
trevormangahas <trevormangahas@gmail.com>
2022-02-27 21:59:14 -0500

    added arrays

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,43 @@
 // Assignment code here
 
+let numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+let charArray = ['!', '#', '$', '=', '?', '@', '&'];
+let uppercaseArray = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+let lowercase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
