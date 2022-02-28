b6971c5bee924c443c9c41f9d149bab87c92326d
trevormangahas <trevormangahas@gmail.com>
2022-02-27 22:09:07 -0500

    added console logs and code to recieve input on length and or special characters

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,28 @@ let charArray = ['!', '#', '$', '=', '?', '@', '&'];
 let uppercaseArray = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
 let lowercase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
 
+// pulls random letters, numbers or signs from arrays
+let passwordNumber = numberArray[Math.floor(Math.random() * numberArray.length)];
+console.log(passwordNumber)
 
+let passwordChar = charArray[Math.floor(Math.random() * charArray.length)];
+console.log(passwordChar)
+
+let passwordUpper = uppercaseArray[Math.floor(Math.random() * uppercaseArray.length)];
+console.log(passwordUpper)
+
+
+function generatePassword() {
+    // getting password length from user input
+    let passwordLength = Math.floor(prompt('How many characters is your password?'))
+    if (passwordLength <= 7 || passwordLength >= 129) {
+        alert('Invalid Number! Please enter a number between 8 and 128.');
+        passwordLength = prompt('How many characters is your password?');
+}
+    // giving user input from prompt to variable, answering in lowercase only
+    let passwordCriteriaOne = prompt('Does your password need special characters? Enter "YES" or  "NO" ').toLocaleLowerCase();
+    let passwordCriteriaTwo = prompt('Does your password need numbers? Enter "YES" or "NO" ').toLocaleLowerCase();
+    let passwordCriteriaThree = prompt('Does your password need Uppercase and Lowercase letters? Enter "YES" or "NO"').toLocaleLowerCase();
 
 
 
